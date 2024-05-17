e06ce7298a68337f354664e4a1297eb979e13381
Oma-yma1 <zgani.omayma@gmail.com>
2024-05-17 17:47:12 +0100

    menu

--- meteo-app/src/components/Navbar/Navbar.tsx
+++ meteo-app/src/components/Navbar/Navbar.tsx
@@ -1,54 +1,66 @@
+import { useEffect } from 'react';
 import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar: React.FC = () => {
-  const [isChecked, setIsChecked] = useState(false);
+    const [openDropdown, setOpenDropdown] = useState<HTMLElement | null>(null);
 
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-  };
+    const handleDropdownClick = (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        const currentDropdown = event.currentTarget.parentElement;
+        if (currentDropdown === openDropdown) {
+            setOpenDropdown(null);
+        } else {
+            setOpenDropdown(currentDropdown);
+        }
+    };
 
-  return (
-    <nav>
-      <div className="navbar">
-        <div className="container nav-container">
-          <input
-            className="checkbox"
-            type="checkbox"
-            name=""
-            id=""
-            checked={isChecked}
-            onChange={handleCheckboxChange}
-          />
-          <div className="hamburger-lines">
-            <span className="line line1"></span>
-            <span className="line line2"></span>
-            <span className="line line3"></span>
-          </div>
-          <div className="logo">
-            <h1>Navbar</h1>
-          </div>
-          <div className="menu-items">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">about</a>
-            </li>
-            <li>
-              <a href="#">blogs</a>
-            </li>
-            <li>
-              <a href="#">portfolio</a>
-            </li>
-            <li>
-              <a href="#">contact</a>
-            </li>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+    const handleDocumentClick = (event: MouseEvent) => {
+        if (openDropdown && !openDropdown.contains(event.target as Node)) {
+            setOpenDropdown(null);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('click', handleDocumentClick);
+        return () => {
+            document.removeEventListener('click', handleDocumentClick);
+        };
+    }, [openDropdown]);
+
+    return (
+        <nav role="navigation" className="nav">
+            <div className="brandName">Meteo Maroc</div>
+            <ul className="navItems">
+                <li className="navItem">
+                    <a href="#" className="navLink"><span>Home</span></a>
+                </li>
+
+                <li className="navItem">
+                    <a href="#" className="navLink"><span>Services</span></a>
+                </li>
+                <li className="navItem">
+                    <a href="#" className="navLink"><span>Pricing</span></a>
+                </li>
+                <li className="navItem">
+                    <a href="#" className="navLink"><span>News</span></a>
+                </li>
+                <li className={`navItem dropdown ${openDropdown?.classList.contains('dropdown') ? 'showSubmenu' : ''}`}>
+                    <a href="#" className="navLink" onClick={handleDropdownClick}><span>More</span></a>
+                    <nav className="submenu">
+                        <ul className="submenuItems">
+                            <li className="submenuItem"><a href="#" className="submenuLink">About</a></li>
+                            <li className="submenuItem"><a href="#" className="submenuLink">Contact</a></li>
+                            <li className="submenuItem"><hr className="submenuSeperator" /></li>
+                            <li className="submenuItem"><a href="#" className="submenuLink">Support</a></li>
+                            <li className="submenuItem"><a href="#" className="submenuLink">FAQs</a></li>
+                            <li className="submenuItem"><a href="#" className="submenuLink">Careers</a></li>
+                        </ul>
+                    </nav>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
