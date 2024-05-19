d5af2d19225d121a2366ac0e00be5f591e761f96
Oma-yma1 <zgani.omayma@gmail.com>
2024-05-19 15:08:00 +0100

    b

--- meteo-app/src/App.tsx
+++ meteo-app/src/App.tsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
+{/*<<<<<<< HEAD*/}
 // import Search from "./components/search-irea";
 // import Map from "./components/map/index";
 import Search from "./components/search-irea/main";
-=======
-import Weather from "./components/weather/Weather"; 
+{/*=======*/}
+{/*import Weather from "./components/weather/Weather";*/}
 /*import Search from "./components/search-irea"; */
 
->>>>>>> 4cd4f8911a1837b28813a49b4f7d031753d5ddd9
+{/*>>>>>>> 4cd4f8911a1837b28813a49b4f7d031753d5ddd9*/}
 
 import "./App.css";
 
@@ -16,13 +16,13 @@ function App() {
   return (
     <>
       <Header />
-<<<<<<< HEAD
+{/*<<<<<<< HEAD*/}
       <Search />
 
 =======
-      <Weather /> {/* Add the Weather component */}
+      {/*<Weather />*/} {/* Add the Weather component */}
       {/*<Search />*/}
->>>>>>> 4cd4f8911a1837b28813a49b4f7d031753d5ddd9
+{/*>>>>>>> 4cd4f8911a1837b28813a49b4f7d031753d5ddd9*/}
       <Footer />
     </>
   );
