47669a6aa73e97688fba1d25104fb9cad84bb75d
Oma-yma1 <zgani.omayma@gmail.com>
2024-05-20 21:20:49 +0100

    ta

--- meteo-app/src/App.tsx
+++ meteo-app/src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-// import Weather from "./components/weather/Weather";
+// immport Weather from "./components/weather/Weather";
 import Search from "./components/search-irea/";
 
 import "./App.css";
