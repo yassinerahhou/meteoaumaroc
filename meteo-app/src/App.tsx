b500b2131212459c15eb83d6cbf37dbaf8b61b69
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-05-22 20:25:24 +0100

    add dynamic map

--- meteo-app/src/App.tsx
+++ meteo-app/src/App.tsx
@@ -1,36 +1,21 @@
-//import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 // immport Weather from "./components/weather/Weather";
 import Search from "./components/search-irea/";
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import Map from "./components/dynamic_map/map";
 
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import "./DarkMode.css"; // Import DarkMode.css
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-    document.body.classList.toggle("dark-mode", !darkMode);
-  };
-
   return (
     <>
-      {/*<Header /> */}
+      <Header />
       {/* <Weather />  */}
-      <Navbar />
-      <div className="dark-mode-toggle" onClick={toggleDarkMode}>
-        {darkMode ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
-
-      </div>
       <Search />
 
+      <Map />
 
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
