43e56db36929d28b2984a806cb3ae52ea8680bb1
Oma-yma1 <zgani.omayma@gmail.com>
2024-05-22 19:23:39 +0100

    dr

--- meteo-app/src/App.tsx
+++ meteo-app/src/App.tsx
@@ -1,16 +1,34 @@
-import Header from "./components/Header";
+//import Header from "./components/Header";
 // import Footer from "./components/Footer";
 // immport Weather from "./components/weather/Weather";
 import Search from "./components/search-irea/";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import "./DarkMode.css"; // Import DarkMode.css
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.body.classList.toggle("dark-mode", !darkMode);
+  };
+
   return (
     <>
-      <Header />
+      {/*<Header /> */}
       {/* <Weather />  */}
+      <Navbar />
+      <div className="dark-mode-toggle" onClick={toggleDarkMode}>
+        {darkMode ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
+
+      </div>
       <Search />
+
 
       {/* <Footer /> */}
     </>
