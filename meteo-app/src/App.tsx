e104e2c1b386107950e48b9e100fcede7f8974f1
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-06-03 21:40:28 +0100

    test app

--- meteo-app/src/App.tsx
+++ meteo-app/src/App.tsx
@@ -1,15 +1,15 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-// immport Weather from "./components/weather/Weather";
-import Search from "./components/search-irea/";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// // immport Weather from "./components/weather/Weather";
+// import Search from "./components/search-irea/";
 import Map from "./components/dynamic_map/map";
 import "./App.css";
 import Header_2 from "./components/Header-2";
 import Footer_2 from "./components/FOOTER-2";
 import Contact from "./components/CONTACT";
-import WeatherDisplay from "./components/search-irea/WeatherDisplay";
-import { weatherDataType } from "./components/search-irea/types";
-import WeatherDisplayProps from "./components/search-irea/WeatherDisplay";
+// import WeatherDisplay from "./components/search-irea/WeatherDisplay";
+// import { weatherDataType } from "./components/search-irea/types";
+// import WeatherDisplayProps from "./components/search-irea/WeatherDisplay";
 // import "./assets/js/main.js";
 function App() {
   return (
